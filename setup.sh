b439e8bef97adb8ea0a571eae0a717cde3ba0207
qw46478 <46646584+qw46478@users.noreply.github.com>
2019-05-09 16:38:30 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo -e "sutup black Hacker "
+pkg install figlet
+pkg install mpv
+echo -e "\033[36m"
+pkg install python
+pkg install python2
+pip install --upgrade pip
+pkg install pip 
+pkg install nano 
+pip install mechanize
+pip install --upgrade pip
+pip2 install mechanize
+pip install Browser
+pip install --upgrade pip
+pip2 install Browser
+pip install --upgrade pip
+pip2 install BeautifulSoup
+pip install --upgrade pip
+pip install BeautifulSoup
+pip install --upgrade pip
+pip install bs4
+pip install --upgrade pip
+pip2 install bs4
+pip install --upgrade pip
+pip install cookielib
+pip install --upgrade pip
+pip2 install cookielib
+pip install --upgrade pip
+pip install random
+pip install --upgrade pip
+pip2 install random
